8be2fb12edf4c9b23914b208dad40154b06683c7
ainura-t <stredeva@gmail.com>
2021-03-02 16:41:47 -0500

    added new file

---
+++ main.js
@@ -0,0 +1,11 @@
+var number_of_children;
+var partners_name;
+var place;
+var job_title;
+
+number_of_children = 3;
+partners_name = "John";
+place = "California";
+job_title = "Accountant";
+
+document.write("You will be an " + job_title + " in " + place + ", and married to " + partners_name + " with " + number_of_children + " kids.");
